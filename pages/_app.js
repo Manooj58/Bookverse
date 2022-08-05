be543118b5e0f12fbb33d5df8f22b9bc16e31468
kamal0620040 <neupane0620040@outlook.com>
2022-08-05 16:38:50 +0545

    Added Banner Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
-import '../styles/globals.css';
 import { Navbar, Footer } from '../components';
 
+import '../styles/globals.css';
+
 const MyApp = ({ Component, pageProps }) => (
-    <ThemeProvider attribute="class">
-      <div className="dark:bg-book-dark bg-white min-h-screen">
-        <Navbar />
-        <Footer />
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-book-dark bg-white min-h-screen">
+      <Navbar />
+      <div className="pt-65">
+        <Component {...pageProps} />
       </div>
-      <Script src="https://kit.fontawesome.com/a7110d492e.js" crossOrigin="anonymous" />
-    </ThemeProvider>
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/a7110d492e.js" crossOrigin="anonymous" />
+  </ThemeProvider>
 );
 
 export default MyApp;
